12a0536b36810c40da24b080a468e5eeab4692b6
abhinav4532raj <156064656+abhinav4532raj@users.noreply.github.com>
2024-01-11 07:30:27 +0000

    object

---
+++ 03_object/ 02_object.js
@@ -0,0 +1,18 @@
+const tinderuser ={}
+
+
+tinderuser.id="123abs"
+tinderuser.name="sammy"
+tinderuser.isloggedin=false
+
+//console.log(tinderuser);
+const regularuser={
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"abhi",
+            lastname:"raj"
+        }
+    }
+}
+console.log(regularuser.fullname);
